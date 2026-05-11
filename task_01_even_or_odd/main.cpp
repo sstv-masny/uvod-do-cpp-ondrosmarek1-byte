417dcccebee646cab832d9845dae5a2cf664082b
MaresekOndrosek <ondros.marek1@gmail.com>
2026-05-11 23:41:41 +0200

    Update main.cpp

--- task_01_even_or_odd/main.cpp
+++ task_01_even_or_odd/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    long long n;
+    long long n;            //zadefinujeme premennu
     if (!(std::cin >> n)) return 0;
     if (n % 2 == 0) {       //testujeme delitelnost 2
         std::cout << "Even\n";
